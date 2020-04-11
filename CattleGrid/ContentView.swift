5c7e23e2f3f2871e6f88894bad46048c6e11db15
Eric Betts <bettse@fastmail.fm>
2020-04-10 20:32:17 -0700

    Initial Commit

---
+++ CattleGrid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CattleGrid
+//
+//  Created by Eric Betts on 4/10/20.
+//  Copyright © 2020 Eric Betts. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
